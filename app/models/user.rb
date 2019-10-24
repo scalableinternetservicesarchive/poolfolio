35c3b7bd3a07c3d491c1f40a04fbb65d1d808969
liuhanyao98 <liuhanyao98@163.com>
2019-10-24 22:12:48 +0000

    Add uniqueness validation in the database and only allow lowercase email addr.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
+  # callback: invoked before the user object is saved.
+  before_save { self.email = email.downcase }
+  
   validates :firstname, presence: true, length: { maximum: 50 };
   validates :lastname, presence: true, length: { maximum: 50 };
   validates :email, presence: true, length: { maximum: 255 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+                    uniqueness: true
 end
