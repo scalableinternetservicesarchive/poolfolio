1b13f34f8565b721873d76144f31d22889c483c9
pmarks98 <piersonmarks@engineering.ucla.edu>
2019-10-31 12:57:04 -0700

    Fixed custom login fields. Added login/logout functionality.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
-      protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 
-      def index
-      	  #render html: "Welcome to Poolfolio"
-      end
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname])
+  end
 end
