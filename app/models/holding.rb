270014bb9952405dc15f49e2e7e8808234f81e17
liuhanyao98 <liuhanyao98@163.com>
2019-11-14 00:39:44 +0000

    add model validations for team and holding

--- app/models/holding.rb
+++ app/models/holding.rb
@@ -1,3 +1,4 @@
 class Holding < ApplicationRecord
     belongs_to :team
+    validates :quantity, numericality: { greater_than_or_equal_to: 0 }
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,7 @@ class Team < ApplicationRecord
   has_many  :users
   has_many  :suggestions
   has_many  :holdings
+  
+  validates :balance, numericality: { greater_than_or_equal_to: 0 }
+  validates :value, numericality: { greater_than_or_equal_to: 0 }
 end
