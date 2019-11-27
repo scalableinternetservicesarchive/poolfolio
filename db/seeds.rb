e13fa339df96bfaa6a0143dd3215cadd4134e90c
Jack Zhang <hijackz337@gmail.com>
2019-11-26 23:29:18 -0800

    Removed QUAN from the stocks

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,20 @@
 # Check so that each newly spawned EB instance doesn't try to seed and already seeded database
 if Team.all.size == 0
-  # Default team which every new user joins
-  first_team = Team.create(name: "The Quants", balance: 4000)
+  # Teams
+  first = Team.create(name: "The Quants", balance: 4000)
   second = Team.create(name: "PYPL", balance: 4000)
   third = Team.create(name: "AMZN", balance: 4000)
   fourth = Team.create(name: "UBER", balance: 4000)
   fifth = Team.create(name: "Microsoft", balance: 4000)
+
+  # Users
   first_user = User.create(firstname: "Jack", lastname: "Zhang", email: "jz@ucla.edu", password: "qwerty", password_confirmation: "qwerty", team_id: 1)
   second_user = User.create(firstname: "Pierson", lastname: "Marks", email: "pm@ucla.edu", password: "qwerty", password_confirmation: "qwerty", team_id: 1)
   third_user = User.create(firstname: "Hanyo", lastname: "Liu", email: "hl@ucla.edu", password: "qwerty", password_confirmation: "qwerty", team_id: 1)
   fourth_user = User.create(firstname: "Nikita", lastname: "Luyanenko", email: "nl@ucla.edu", password: "qwerty", password_confirmation: "qwerty", team_id: 1)
   fourth_user = User.create(firstname: "Nikita", lastname: "Luyanenko", email: "nikita1923666@gmai.com", password: "warlords", password_confirmation: "warlords", team_id: 1)
-  first_suggestion1 = Suggestion.create(quantity: 500, team_id: 1, user_id: 1, ticker: 'QUAN')
-  first_suggestion2 = Suggestion.create(quantity: 500, team_id: 2, user_id: 2, ticker: 'QUAN')
-  first_suggestion3 = Suggestion.create(quantity: 500, team_id: 3, user_id: 3, ticker: 'QUAN')
-  first_suggestion4 = Suggestion.create(quantity: 500, team_id: 4, user_id: 4, ticker: 'QUAN')
-  first_suggestion5 = Suggestion.create(quantity: 500, team_id: 5, user_id: 5, ticker: 'QUAN')
+
+  # Suggestions
   second_suggestion1 = Suggestion.create(quantity: 500, team_id: 1, user_id: 1, ticker: 'PYPL')
   second_suggestion2 = Suggestion.create(quantity: 500, team_id: 2, user_id: 2, ticker: 'PYPL')
   second_suggestion3 = Suggestion.create(quantity: 500, team_id: 3, user_id: 3, ticker: 'PYPL')
@@ -31,6 +30,7 @@ if Team.all.size == 0
   five_suggestion3 = Suggestion.create(quantity: 500, team_id: 3, user_id: 3, ticker: 'UBER')
   five_suggestion4 = Suggestion.create(quantity: 500, team_id: 4, user_id: 4, ticker: 'UBER')
   five_suggestion5 = Suggestion.create(quantity: 500, team_id: 5, user_id: 5, ticker: 'UBER')
+
   # Stocks using Alpaca's Market API
   res = RestClient.get("https://paper-api.alpaca.markets/v2/assets", :"APCA-API-KEY-ID" => "PKI9MD0KBM4B957XF9IJ", :"APCA-API-SECRET-KEY" => "DxnPxsIzEZDwFcgfnlHoSKhkKFsfEgsHFkggmcQx")
   res = JSON.parse(res)
