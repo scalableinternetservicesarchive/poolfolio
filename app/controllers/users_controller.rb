35465661ab476ab1caafe92c6b01ca585a2b76df
Jack Zhang <hijackz337@gmail.com>
2019-12-04 17:44:44 -0800

    Scroll

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
 
     def set_teams
       @team = Team.find(current_user.team_id)
-      @teams = Team.all.sort_by{ |team| team.balance + team.value}.reverse[0...19]
+      @teams = Team.all.sort_by{ |team| team.balance + team.value}.reverse
     end
 
     def check_user
